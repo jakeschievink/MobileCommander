5eeea005354dee884df5582d908d263ea7765c2a
Jake Schievink <jakeschievink@gmail.com>
2013-12-27 19:56:05 -0500

    added better styling

--- mainscreen.py
+++ mainscreen.py
@@ -8,7 +8,7 @@ from PyQt4.QtGui import *
 from threading import Thread, Event
 
 pcommands = ["Read", "Write", "Draw","Rap","Meditate"]
-dcommands = ["Jerk Off!", "Buy Some Shit", "Jerk Off!", "Get some money", "Score some coke!"]
+dcommands = ["Jerk Off!", "Buy Some Shit", "Jerk Off!", "Get some money", "Score some coke!", "Watch Porn!"]
 
 class Commands(QThread): 
 
@@ -29,32 +29,36 @@ class Commands(QThread):
                     self.emit(SIGNAL('timefinished'))
                     self.terminate()
 
-class Form(QDialog):
+class CommandWindow(QDialog):
    
     def __init__(self, parent=None):
-        print "initialized"
-        super(Form, self).__init__(parent)
-
-        self.commandtext = QLabel("<font size=200>"+getCommand()+"</font>")
+        super(CommandWindow, self).__init__(parent)
+        self.commandtext = QLabel("Command")
         self.timetext = QLabel("Time")
         layout = QVBoxLayout()
         layout.addWidget(self.commandtext)
         layout.addWidget(self.timetext)
         self.commandtext.setAlignment(Qt.AlignCenter)
         self.timetext.setAlignment(Qt.AlignCenter)
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(p)
         self.setLayout(layout)
         self.setWindowFlags(Qt.SplashScreen)
         self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.timetext.setStyleSheet('font-size: 200px; color: black;')
+        self.commandtext.setStyleSheet('font-size: 100px; color: black')
 
     def setTimeText(self, time):
         p1,p2 = divmod(int(time), 60)
         if p2 < 10: p2 = "0" + str(p2)
-        self.timetext.setText("<font size=200>"+str(p1)+":"+str(p2)+"</font>")
+        self.timetext.setText(str(p1)+":"+str(p2))
 
     def setCommandText(self, message):
-        self.commandtext.setText("<font size=300>"+str(message)+"</font>")
+        self.commandtext.setText(str(message))
 
-    def sleepyTime(self, delay=4):
+    def sleepyTime(self):
         print "sleeping"
         self.hide()
         time.sleep(float(delayTime))
@@ -77,10 +81,9 @@ def toMinutes(num):
     return num*60
 
 if __name__ == '__main__':
-        
     argparser = argparse.ArgumentParser(description='pass some integers')
-    argparser.add_argument('-dt','--delaytime', metavar='N', type=int, default=25, help="please input two integerst, first is delaytime second is showtime")
-    argparser.add_argument('-st','--showtime', metavar='N', type=int, default=5, help="please input two integerst, first is delaytime second is showtime")
+    argparser.add_argument('-dt','--delaytime', metavar='N', type=int, default=25, help="input delay time")
+    argparser.add_argument('-st','--showtime', metavar='N', type=int, default=5, help="input show time")
     argparser.add_argument('-dys', '--dystopian', action='store_true')
     args = argparser.parse_args()
     if(args.dystopian):
@@ -88,13 +91,12 @@ if __name__ == '__main__':
         commands = dcommands
     else: 
         commands = pcommands
-
-
     delayTime = toMinutes(args.delaytime)
     showTime = toMinutes(args.showtime)
+
     app = QApplication(sys.argv)
-    form = Form()   
-    form.sleepyTime()
+    window = CommandWindow()   
+    window.sleepyTime()
     app.exec_()
 
 
